c7f3ff886a3413280099fb8a776ab13f9775ecba
Prabhakar Beniwal <prabhakarbeniwal4@gmail.com>
2023-08-27 20:38:02 +0530

    Solve the error in deployment of user

--- src/component/Home/ProductCard.js
+++ src/component/Home/ProductCard.js
@@ -8,9 +8,12 @@ const ProductCard = ({ product }) => {
     readOnly: true,
     precision: 0.5,
   };
+
+  console.log(product)
+
   return (
     <Link className="productCard" to={`/product/${product._id}`}>
-      <img src={product.images[0].url} alt={product.name} />
+      { product.images.length && <img src={product.images[0].url} alt={product.name} /> }
       <p>{product.name}</p>
       <div>
         <Rating {...options} />{" "}
