56d6e8522fc20c069ffb972d9bf05965430a3f22
Prabhakar Beniwal <prabhakarbeniwal4@gmail.com>
2023-10-03 18:19:56 +0530

    footer update

--- src/component/layout/About/About.js
+++ src/component/layout/About/About.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./aboutSection.css";
 import { Button, Typography, Avatar } from "@material-ui/core";
-import YouTubeIcon from "@material-ui/icons/YouTube";
+
 import InstagramIcon from "@material-ui/icons/Instagram";
 const About = () => {
   const visitInstagram = () => {
-    window.location = "https://instagram.com/meabhisingh";
+    window.location = "https://instagram.com/its_me_prabhakar_048?igshid=OGQ5ZDc2ODk2ZA==";
   };
   return (
     <div className="aboutSection">
@@ -16,30 +16,20 @@ const About = () => {
 
         <div>
           <div>
-            <Avatar
-              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
-              src="https://res.cloudinary.com/tripleayt/image/upload/v1631555947/products/jpyibarlaxawvcvqjv5b.png"
-              alt="Founder"
-            />
-            <Typography>Abhishek Singh</Typography>
+            
+            <Typography>Prabhakar</Typography>
             <Button onClick={visitInstagram} color="primary">
               Visit Instagram
             </Button>
             <span>
-              This is a sample wesbite made by @meabhisingh. Only with the
-              purpose to teach MERN Stack on the channel 6 Pack Programmer
+              This is a sample wesbite made by me by using mern stack. 
             </span>
           </div>
           <div className="aboutSectionContainer2">
             <Typography component="h2">Our Brands</Typography>
-            <a
-              href="https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw"
-              target="blank"
-            >
-              <YouTubeIcon className="youtubeSvgIcon" />
-            </a>
+            
 
-            <a href="https://instagram.com/meabhisingh" target="blank">
+            <a href="https://instagram.com/its_me_prabhakar_048?igshid=OGQ5ZDc2ODk2ZA==" target="blank">
               <InstagramIcon className="instagramSvgIcon" />
             </a>
           </div>
--- src/component/layout/Contact/Contact.js
+++ src/component/layout/Contact/Contact.js
@@ -5,9 +5,7 @@ import { Button } from "@material-ui/core";
 const Contact = () => {
   return (
     <div className="contactContainer">
-      <a className="mailBtn" href="mailto:mymailforabhi@gmail.com">
-        <Button>Contact: mymailforabhi@gmail.com</Button>
-      </a>
+        <Button>Contact: prabhakarbeniwal4@gmail.com</Button>
     </div>
   );
 };
